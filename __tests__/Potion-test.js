4c5811d97a12113f7a4578e81219ccbfcdbf6284
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-07-05 23:09:30 -0700

    compared code

--- __tests__/Potion-test.js
+++ __tests__/Potion-test.js
@@ -1,11 +1,16 @@
-const { default: test } = require('node:test');
-const Potion = require('../lib/Potion');
+const Potion = require('../lib/Potion.js');
 
+test('creates a health potion object', () => {
+  const potion = new Potion('health');
 
-test('create a health potion object', () => {
-    const potion = new Potion('heath');
-
-    expect(potion.name).toBe('health');
-    expect(potion.value).toEqual(expect.any(Number));
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
 });
-expect
+
+test('creates a random potion object', () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
